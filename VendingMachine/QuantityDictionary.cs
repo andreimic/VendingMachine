08932ed8ba0a4d3f7e867137cfe2034bb178a6b8
Andrei Mustata <a.mustata@levi9.com>
2017-03-01 18:50:09 +0200

    replace internal quantitydictionary implementation with actual dictionary

--- VendingMachine/QuantityDictionary.cs
+++ VendingMachine/QuantityDictionary.cs
@@ -1,28 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 namespace VendingMachine
 {
     public class QuantityDictionary
     {
-        private int[] _quantityKeys = { };
-        private int[] _quantityValues = { };
+        private readonly Dictionary<int, int> _quantities = new Dictionary<int, int>();
 
         public void SetQuantity(int quantity, int value)
         {
-            _quantityValues[Array.IndexOf(_quantityKeys, quantity)] = value;
+            _quantities[quantity] = value;
         }
 
         public int GetQuantity(int value)
         {
-            return _quantityValues[Array.IndexOf(_quantityKeys, value)];
+            return _quantities[value];
         }
 
         public void Add(int c, int n)
         {
-            Array.Resize(ref _quantityKeys, _quantityKeys.Length + 1);
-            Array.Resize(ref _quantityValues, _quantityValues.Length + 1);
-            _quantityKeys[_quantityKeys.Length - 1] = c;
-            _quantityValues[_quantityValues.Length - 1] = n;
+            _quantities[c] = n;
         }
     }
 }
